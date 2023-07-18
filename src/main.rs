68f28555cfefef8b49a9eecd0e8d19089b33dbbb
o0Ignition0o <jeremy.lempereur@gmail.com>
2023-07-18 12:38:41 +0200

    axum json

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, routing::post, Router, Json, extract};
 use std::net::SocketAddr;
+use serde::{Serialize, Deserialize};
 
-async fn hello() -> &'static str {
-    "Hello, World!"
+async fn hello() -> String {
+    "Hello, World!".to_string()
+}
+
+#[derive(Serialize)]
+struct HelloJson {
+    sender: String,
+    receiver: String,
+    message: String
+}
+
+#[derive(Deserialize)]
+struct HelloPayload {
+    sender: String,
+    receiver: String,
+}
+
+async fn say_hello_json(
+    extract::Json(hello_payload): extract::Json<HelloPayload>,
+) -> Json<HelloJson> {
+    let message = format!(
+        "{} says hello to {}",
+        hello_payload.sender, hello_payload.receiver
+    );
+    Json(HelloJson {
+        sender: hello_payload.sender,
+        receiver: hello_payload.receiver,
+        message,
+    })
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello));
+    let app = Router::new().route("/", get(hello)).route("/json", post(say_hello_json));
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
